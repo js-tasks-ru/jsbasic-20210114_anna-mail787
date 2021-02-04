3de498711585ad8e6ccd89769a7001d47360455d
Anna Mironovich <anna.mail787@gmail.com>
2021-02-04 21:05:34 +0300

    module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,8 +1,6 @@
 function toggleText() {
   const toggleButton = document.querySelector('.toggle-text-button');
-  console.log(toggleButton);
   const text = document.querySelector('#text');
-  console.log(text);
   toggleButton.addEventListener('click', function() {
     text.toggleAttribute('hidden');
   })

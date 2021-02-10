4361faaa62d9f4b15731ef9c96e76ae7a86df68c
Anna Mironovich <anna.mail787@gmail.com>
2021-02-10 16:36:40 +0300

    6-module 1-task v1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -29,6 +29,53 @@
  * @constructor
  */
 export default class UserTable {
+
   constructor(rows) {
+    this.elem = document.createElement('table');
+    const thead = document.createElement('thead');
+    this.elem.append(thead);
+    const theadRow = document.createElement('tr');
+    thead.append(theadRow);
+    let th = document.createElement('th');
+    th.textContent = 'Имя';
+    theadRow.append(th);
+    th.textContent = 'Возраст';
+    theadRow.append(th);
+    th.textContent = 'Зарплата';
+    theadRow.append(th);
+    th.textContent = 'Город';
+    theadRow.append(th);
+    th.textContent = '';
+    theadRow.append(th);
+    const tbody = document.createElement('tbody');
+    this.elem.append(tbody);
+
+    for (let row of rows) {
+      const tr = document.createElement('tr');
+      tbody.append(tr);
+      for (let key in row) {
+        const td = document.createElement('td');
+        td.textContent = row[key];
+        tr.append(td);
+      }
+      const buttonTd = document.createElement('td');
+      buttonTd.innerHTML = '<button class="removeButton">X</button>';
+      tr.append(buttonTd);
+    }
+
+    this.clickHandler = this.clickHandler.bind(this);
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.elem.addEventListener('click', this.clickHandler);
+  }
+
+  clickHandler(event) {
+    if (event.target.className != 'removeButton') return;
+
+      let tr = event.target.closest('tr');
+      tr.remove();
+
   }
 }
